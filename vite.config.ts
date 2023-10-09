1041225dcc2700dce18332c4b472ce323d4a15a5
Vova Puchkov <volodymyr.puchkov@netreveal.ai>
2023-10-09 13:37:03 +0300

    feat(test): add basic module and test with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'my-lib',
+      fileName: 'my-lib',
+    },
+  },
+  plugins: [react(), dts()],
+});
