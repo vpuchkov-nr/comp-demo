d35117ab837cca2a565efa7a576e7fdea2fe5adc
Vova Puchkov <volodymyr.puchkov@netreveal.ai>
2023-10-10 09:05:39 +0300

    config(vite): use rollup options instead of external deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react-swc'
-import createExternal from 'vite-plugin-external';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,15 +11,22 @@ export default defineConfig({
       name: 'my-lib',
       fileName: 'my-lib',
     },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
   },
   plugins: [
     react(),
     dts(),
-    createExternal({
-      externals: {
-        react: 'react',
-        'react-dom': 'react-dom',
-      }
-    })
   ],
 });
