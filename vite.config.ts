7bf5356d3472b5bddabafd4d7d1809429a99cfff
Vova Puchkov <volodymyr.puchkov@netreveal.ai>
2023-10-10 09:24:07 +0300

    config(npm, github): add workflow to publish on release

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react-swc'
+import typescript from "@rollup/plugin-typescript";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    reportCompressedSize: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'my-lib',
@@ -23,6 +26,16 @@ export default defineConfig({
           'react-dom': 'ReactDOM',
         },
       },
+      // plugins: [
+      //   typescriptPaths({
+      //     preserveExtensions: true,
+      //   }),
+      //   typescript({
+      //     sourceMap: false,
+      //     declaration: true,
+      //     outDir: "dist",
+      //   }),
+      // ],
     },
   },
   plugins: [
