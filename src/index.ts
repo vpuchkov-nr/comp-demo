6d955c930a7a2c47186143a428e354b59b16148d
Vova Puchkov <volodymyr.puchkov@netreveal.ai>
2023-10-11 14:39:58 +0300

    feat(components): add Badge component; add chromatic workflow

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-export { default as sum } from './sum';
-export { default as Button } from './Button';
+export { default as sum } from './sum.ts';
+export { default as Button } from './Button/Button.tsx';
