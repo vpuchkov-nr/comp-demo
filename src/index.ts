cd1a3856b51eb24effe9a17a11321e420f32934b
Vova Puchkov <volodymyr.puchkov@netreveal.ai>
2023-10-12 14:20:51 +0300

    chore(): reexport ThemeProvider

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import './tailwind.css';
 // import tailwindConfig from '../tailwind.config.js';
 // tailwind.config = tailwindConfig;
 export { default as Button } from './Button/Button.tsx';
+export { default as ThemeProvider } from './ThemeProvider/';
